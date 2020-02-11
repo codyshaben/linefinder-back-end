f4175ed086c0edb0efa0996db0e8856c52eb230a
Cody Shaben <cshaben87@gmail.com>
2020-02-11 15:39:08 -0700

    attempt 1 at fixing heroku

--- routes/trails.js
+++ routes/trails.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fetch = require('node-fetch');
 require('dotenv').config()
 
+
 router.get('/', async (req, res) => {
     const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
     const fetch_response = await fetch(rei_api_url);
