3e5f3c878787cba9b2e98e94914a13bd169a06fe
Cody Shaben <cshaben87@gmail.com>
2020-02-18 00:47:41 -0700

    added endpoints for filtered trails based on star rating

--- routes/trails.js
+++ routes/trails.js
@@ -11,4 +11,49 @@ router.get('/', async (req, res) => {
     res.json(json)
 })
 
+router.get('/one-star', async (req, res) => {
+    const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
+    const fetch_response = await fetch(rei_api_url);
+    const json = await fetch_response.json()
+    res.json(json.trails.filter(trail => {
+        return trail.stars < 2
+    }))
+})
+
+router.get('/two-stars', async (req, res) => {
+    const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
+    const fetch_response = await fetch(rei_api_url);
+    const json = await fetch_response.json()
+    res.json(json.trails.filter(trail => {
+        return trail.stars < 3
+    }))
+})
+
+router.get('/three-stars', async (req, res) => {
+    const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
+    const fetch_response = await fetch(rei_api_url);
+    const json = await fetch_response.json()
+    res.json(json.trails.filter(trail => {
+        return trail.stars < 4 && trail.stars > 3
+    }))
+})
+
+router.get('/four-stars', async (req, res) => {
+    const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
+    const fetch_response = await fetch(rei_api_url);
+    const json = await fetch_response.json()
+    res.json(json.trails.filter(trail => {
+        return trail.stars < 5 && trail.stars > 4
+    }))
+})
+
+router.get('/five-stars', async (req, res) => {
+    const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
+    const fetch_response = await fetch(rei_api_url);
+    const json = await fetch_response.json()
+    res.json(json.trails.filter(trail => {
+        return trail.stars === 5
+    }))
+})
+
 module.exports = router
