a3e9bc20ddc3438eacc18c54b0d55c9d81387c3c
Cody Shaben <cshaben87@gmail.com>
2020-02-25 13:45:02 -0700

    commented back in trail_data file

--- db/trail_data.js
+++ db/trail_data.js
@@ -1,17 +1,17 @@
-// require('dotenv').config()
-// const fetch = require('node-fetch');
-// var knex = require('./knex')
+require('dotenv').config()
+const fetch = require('node-fetch');
+var knex = require('./knex')
 
-// const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
+const rei_api_url = `https://www.powderproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=1000&maxResults=500&key=${process.env.REI_API_KEY}`;
 
-// const getTrails = () => {
-//     fetch(rei_api_url)
-//         .then(response => response.json())
-//         .then(data => {
-//             knex.insert(data.trails).into('trail').then(function (id) {
-//                 console.log(id)
-//             })
-//         })
-// }
+const getTrails = () => {
+    fetch(rei_api_url)
+        .then(response => response.json())
+        .then(data => {
+            knex.insert(data.trails).into('trail').then(function (id) {
+                console.log(id)
+            })
+        })
+}
 
-// getTrails()
+getTrails()
